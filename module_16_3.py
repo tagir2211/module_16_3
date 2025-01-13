074407f632c1161a694e46ee6ee8282536a86a72
tagir2211 <tagir2211@gmail.com>
2025-01-13 15:38:41 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -1,12 +1,11 @@
-from typing import Union
+from typing import Annotated
 
 from fastapi import FastAPI, Path
 
-from typing import Annotated
-
-users = {'1': {'username': 'Example', 'age': 18}}
+users = {'1': 'username: Example , age: 18'}
 
 app = FastAPI()
+
 
 @app.get('/users/')
 async def get_users() -> dict:
@@ -17,48 +16,52 @@ async def get_users() -> dict:
 
 
 @app.post('/user/{username}/{age}')
-async def create_user(username: Annotated [str, Path(min_lenght = 5,
-                                                  max_lenght = 20,
-                                                  description = 'Имя пользователя',
-                                                  example = 'Urban')],
-                      age: Annotated [int, Path(le = 120,
-                                               ge = 16,
-                                               description = 'Возраст пользователя',
-                                               example = 30)]) -> str:
+async def create_user(username: Annotated[str, Path(min_lenght=5,
+                                                    max_lenght=20,
+                                                    description='Имя пользователя',
+                                                    example='Urban')],
+                      age: Annotated[int, Path(le=120,
+                                               ge=16,
+                                               description='Возраст пользователя',
+                                               example=30)]) -> str:
     '''
     запрос который добавляет в словарь по максимальному по значению ключом значение строки "Имя: {username}, возраст: {age}". 
     И возвращает строку "User <user_id> is registered".
     '''
-    user_id = int(max(next(reversed(users)))) + 1 
-    users[str(user_id)] = {'username': username, 'age': age}
+    user_id = int(max(next(reversed(users)))) + 1
+    users[str(user_id)] = f'username: {username}, age: {age}'
     return f'User {user_id} is registered'
 
+
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: Annotated [int, Path(ge = 1)],
-                    username: Annotated [str, Path(min_lenght = 5,
-                                                      max_lenght = 20,
-                                                      description = 'Имя пользователя',
-                                                      example = 'Urban')],
-                    age: Annotated [int, Path(le = 120,
-                                               ge = 16,
-                                               description = 'Возраст пользователя',
-                                               example = 30)]) -> str:
+async def update_user(user_id: Annotated[int, Path(ge=1)],
+                      username: Annotated[str, Path(min_lenght=5,
+                                                    max_lenght=20,
+                                                    description='Имя пользователя',
+                                                    example='Urban')],
+                      age: Annotated[int, Path(le=120,
+                                               ge=16,
+                                               description='Возраст пользователя',
+                                               example=30)]) -> str:
     '''
     запрос который обновляет значение из словаря users под ключом user_id на строку "Имя: {username}, возраст: {age}". 
     И возвращает строку "The user <user_id> is updated"
     '''
-    users[str(user_id)] = {'username': username, 'age': age}
-    return f'The user {user_id} is updated'
+    if user_id in users.keys():
+        users[str(user_id)] = f'username: {username}, age: {age}'
+        return f'The user {user_id} is updated'
+    else:
+        return f'The user {user_id} is not found.'
+
 
 @app.delete('/user/{user_id}')
-async def delit_user(user_id: Annotated [int, Path(ge = 1)]) -> str:
+async def delit_user(user_id: Annotated[int, Path(ge=1)]) -> str:
     '''
     запрос который удаляет из словаря users по ключу user_id пару.
     И возвращает строку "The user <user_id> is delited"
     '''
     if str(user_id) in users.keys():
         users.pop(str(user_id))
-        return f'The user {user_id} is delited'
+        return f'The user {user_id} is deleted'
     else:
         return f'The user {user_id} is not found'
-    
